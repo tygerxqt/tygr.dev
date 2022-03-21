ffc4f9ca429d6c0bf046f11e2b626de861917bb5
Ty Mason <tygerxqt@gmail.com>
2022-03-21 22:51:53 +0000

    Add redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,51 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/github',
+        destination: 'https://github.com/tygerxqt',
+        permanent: true,
+      },
+      {
+        source: '/twitter',
+        destination: 'https://twitter.com/tygerxqt',
+        permanent: true,
+      },
+      {
+        source: '/instagram',
+        destination: 'https://instagram.com/tygerxqt',
+        permanent: true,
+      },
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/BJ8sWHntYb',
+        permanent: true,
+      },
+      {
+        source: '/youtube',
+        destination: 'https://www.youtube.com/channel/UCDCBrrYkHrt4cFnoA2SKMdA',
+        permanent: true,
+      },
+      {
+        source: '/twitch',
+        destination: 'https://twitch.tv/tygerxqt',
+        permanent: true,
+      },
+      {
+        source: '/mail',
+        destination: 'mailto:tygerxqt@nordstud.io',
+        permanent: true,
+      },
+      {
+        source: '/donate',
+        destination: 'https://ko-fi.com/tygerxqt',
+        permanent: true,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
