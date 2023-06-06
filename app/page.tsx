79d6658f6c8fa7ca2faf8c666299c93afc078530
Ty Mason <hi@tygr.dev>
2023-06-07 00:14:52 +0100

    feat: add social icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import { Icons } from "@/components/icons";
 import Image from "next/image";
-import { FaReact, FaRust } from "react-icons/fa";
+import { FaReact, FaRust, FaTwitter } from "react-icons/fa";
 import { SiAstro, SiFigma, SiSvelte, SiTypescript } from "react-icons/si";
-import { AiFillCamera, AiFillHtml5 } from "react-icons/ai";
+import { AiFillCamera, AiFillGithub, AiFillHtml5, AiFillInstagram } from "react-icons/ai";
 import { ArrowUpRight, LayoutGrid } from "lucide-react"
 import { MdDesignServices } from "react-icons/md";
 import { Balancer } from "react-wrap-balancer";
@@ -16,7 +16,23 @@ export default function Home() {
     <main className="flex flex-col gap-4 py-8">
       <div className="flex flex-row gap-4">
         <Image width={132} height={132} className="max-h-[132px] rounded-md hidden sm:block" src="https://secure.gravatar.com/avatar/871c2885d0acbbc08be33547816255e3?size=512" alt="Avatar" />
-
+        <div className="flex flex-col items-center gap-2 pt-2 sm:hidden">
+          <Link href="https://github.com/tygerxqt">
+            <Button size={"icon"} className="flex flex-row items-center gap-2 h-9">
+              <AiFillGithub />
+            </Button>
+          </Link>
+          <Link href="https://github.com/tygerxqt">
+            <Button size={"icon"} className="flex flex-row items-center gap-2 h-9">
+              <FaTwitter fill="currentColor" />
+            </Button>
+          </Link>
+          <Link href="https://github.com/tygerxqt">
+            <Button size={"icon"} className="flex flex-row items-center gap-2 h-9 ">
+              <AiFillInstagram />
+            </Button>
+          </Link>
+        </div>
         <div className="flex flex-col items-start">
           <Balancer>
             <small className="text-sm text-neutral-500">
@@ -27,13 +43,13 @@ export default function Home() {
               <span className="dark:text-neutral-400 text-neutral-600">{" "} & {" "}</span>
               head of design
               <span className="dark:text-neutral-400 text-neutral-600">{" "} at {" "}</span>
-              <a target="_blank" href="https://nordstud.io" className="inline-flex flex-row gap-3 items-center hover:text-neutral-800 dark:hover:text-neutral-200">nord studio
+              <a target="_blank" href="https://nordstud.io" className="inline-flex flex-row items-center gap-3 hover:text-neutral-800 dark:hover:text-neutral-200">nord studio
                 <span className="pr-1 dark:text-neutral-400 text-neutral-600">
                   <Icons.Nord className="w-8 h-8" />
                 </span>
               </a>
               <span className="dark:text-neutral-400 text-neutral-600"> and {" "}</span>
-              <a target="_blank" href="https://lofu.studio" className="inline-flex flex-row gap-2 items-center hover:text-neutral-800 dark:hover:text-neutral-200">
+              <a target="_blank" href="https://lofu.studio" className="inline-flex flex-row items-center gap-2 hover:text-neutral-800 dark:hover:text-neutral-200">
                 lofu studio
                 <span>
                   <Icons.Lofu className="w-8 h-8 dark:text-neutral-400 text-neutral-600" />
@@ -44,7 +60,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="hidden flex-row justify-between pt-4 sm:flex">
+      <div className="flex-row justify-between hidden pt-4 sm:flex">
         <div className="flex flex-col gap-3">
           <p className="text-xl font-semibold">
             Languages
@@ -119,11 +135,11 @@ export default function Home() {
         </div>
       </div>
 
-      <hr className="my-4 w-full border border-black/10 dark:border-white/10" />
+      <hr className="w-full my-4 border border-black/10 dark:border-white/10" />
 
       <div className="flex flex-col gap-8">
         <div className="flex flex-col gap-4">
-          <div className="flex flex-col gap-2 items-start sm:justify-between sm:flex-row">
+          <div className="flex flex-col items-start gap-2 sm:justify-between sm:flex-row">
             <h2 className="text-2xl font-bold">
               ~/journey.txt
             </h2>
@@ -131,7 +147,7 @@ export default function Home() {
               A little on how I got to where I am today.
             </small>
           </div>
-          <TextBlockWrapper className="p-2 rounded-md border border-black/10 dark:border-white/10" expandButtonTitle="Read more">
+          <TextBlockWrapper className="p-2 border rounded-md border-black/10 dark:border-white/10" expandButtonTitle="Read more">
             <p>
               I discovered Discord in 2018 and regularly used the various bots on the platform. After using them consistently, I was curious about how they worked, so I asked Google. I looked at multiple JavaScript tutorials and tried to create my bot for people to use.
               <br /><br />
@@ -148,7 +164,7 @@ export default function Home() {
         </div>
 
         <div className="flex flex-col gap-2">
-          <div className="flex flex-row justify-between items-center w-full">
+          <div className="flex flex-row items-center justify-between w-full">
             <div className="flex flex-col items-start">
 
               <h2 className="text-2xl font-bold">
@@ -159,7 +175,7 @@ export default function Home() {
               </small>
             </div>
             <Link href="/projects">
-              <Button size="icon" className="flex flex-row gap-2 items-center w-9 h-9">
+              <Button size="icon" className="flex flex-row items-center gap-2 w-9 h-9">
                 <LayoutGrid className="p-0" />
               </Button>
             </Link>
