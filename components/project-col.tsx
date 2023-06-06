941cb64cf90821c8d72aa684a5d11bc6b4f00974
Ty Mason <hi@tygr.dev>
2023-06-06 23:49:33 +0100

    refactor: project cols to fit mobile

--- components/project-col.tsx
+++ components/project-col.tsx
@@ -1,16 +1,18 @@
-export default function ProjectCol({ href, title, desc, year }: { href: string, title: string, desc: string, year: number}) {
+export default function ProjectCol({ href, title, desc, year }: { href: string, title: string, desc: string, year: number }) {
     return (
         <>
-            <a className="flex flex-row items-center p-2 w-full rounded-md hover:bg-neutral-200" target="_blank" href={href}>
+            <a className="flex flex-row items-center w-full p-2 overflow-hidden rounded-md whitespace-nowrap text-ellipsis hover:bg-neutral-200 dark:hover:bg-neutral-800" target="_blank" href={href}>
                 <p className="gap-1 font-semibold">
                     {title} {" "}
-                    <span className="text-sm font-normal text-neutral-500">
+                    <span className="inline-block text-sm font-normal text-neutral-500">
                         • {desc}
                     </span>
                 </p>
-                <hr className="flex-1 mx-2 border border-black/10 dark:border-white/10" />
-                <span className="text-sm font-medium text-neutral-500">
-                    {year}
+                <span className="hidden w-full sm:flex sm:flex-row sm:items-center ">
+                    <hr className="flex-1 mx-2 border border-black/10 dark:border-white/10" />
+                    <span className="text-sm font-medium text-neutral-500">
+                        {year}
+                    </span>
                 </span>
             </a>
         </>
