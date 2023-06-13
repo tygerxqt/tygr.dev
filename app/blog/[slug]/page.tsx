6c627849af448db69773db71cf14527c23265684
tygerxqt <hi@tygr.dev>
2023-06-13 00:18:12 +0000

    feat: add notion block fetching

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,9 +1,200 @@
-export default function BlogPost() {
+import { notion } from "@/lib/notion";
+import { RichText } from "@/types/block";
+import { Block } from "@/types/block";
+import { BlogPost } from "@/types/blog-post";
+import { Fragment } from "react";
+import { text } from "stream/consumers";
+
+// function Text({ block }: { block: Block }) {
+//     if (!block || !block.paragraph) return null;
+
+//     return block.paragraph.rich_text.map((p, i) => {
+//         return (
+//             <span key={i} className={[
+//                 p.annotations.bold ? "" : "",
+//                 p.annotations.code ? "" : "",
+//                 p.annotations.italic ? "" : "",
+//                 p.annotations.strikethrough ? "" : "",
+//                 p.annotations.underline ? "" : "",
+//             ].join(" ")}>
+//                 {p.text.link ? <a href={p.text.link.url}>{p.text.content}</a> : p.text.content}
+//             </span>
+//         )
+//     })
+// }
+
+function Text({ rich_text }: { rich_text: RichText[] }) {
+    const elements = rich_text.map((value, i) => {
+        const {
+            annotations: { bold, code, color, italic, strikethrough, underline },
+            text,
+        } = value;
+
+        return (
+            <span
+                key={i}
+                className={[
+                    bold ? "" : "",
+                    code ? "" : "",
+                    italic ? "" : "",
+                    strikethrough ? "" : "",
+                    underline ? "" : "",
+                ].join(" ")}
+                style={color !== "default" ? { color } : {}}
+            >
+                {text.link ? <a href={text.link.url}>{text.content}</a> : text.content}
+            </span>
+        );
+    });
+
+    return elements[0];
+};
+
+const renderBlock = (block: Block) => {
+    const { type, id, } = block;
+
+    switch (type) {
+        case "paragraph":
+            return (
+                <p>
+                    <Text rich_text={block.paragraph.rich_text} />
+                </p>
+            );
+        case "heading_1":
+            return (
+                <h1>
+                    <Text rich_text={block.heading_1.rich_text} />
+                </h1>
+            );
+        case "heading_2":
+            return (
+                <h2>
+                    <Text rich_text={block.heading_2.rich_text} />
+                </h2>
+            );
+        case "heading_3":
+            return (
+                <h3>
+                    <Text rich_text={block.heading_3.rich_text} />
+                </h3>
+            );
+        case "bulleted_list_item":
+            return (
+                <li>
+                    <Text rich_text={block.bulleted_list_item.rich_text} />
+                </li>
+            );
+        case "numbered_list_item":
+            return (
+                <li>
+                    <Text rich_text={block.numbered_list_item.rich_text} />
+                </li>
+            );
+        case "to_do":
+            return (
+                <div>
+                    <label htmlFor={id}>
+                        <input type="checkbox" id={id} defaultChecked={block.to_do.checked} />{" "}
+                        <Text rich_text={block.to_do.rich_text} />
+                    </label>
+                </div>
+            );
+        // case "toggle":
+        //     return (
+        //         <details>
+        //             <summary>
+        //                 <Text rich_text={block.paragraph.rich_text} />
+        //             </summary>
+        //             {block.children?.map((block) => (
+        //                 <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+        //             ))}
+        //         </details>
+        //     );
+        // case "child_page":
+        //     return <p>{value.title}</p>;
+        case "image":
+            const src =
+                block.image.type === "external" ? block.image.external.url : block.image.file.url;
+            const caption = ""
+
+            return (
+                <figure>
+                    <img src={src} alt={caption} />
+                    {caption && <figcaption>{caption}</figcaption>}
+                </figure>
+            );
+        default:
+            console.log(`Unsupported block (${type === "unsupported" ? "unsupported by Notion API" : type})`);
+            return <div />;
+    }
+};
+
+async function fetchBlocks(slug: string) {
+    const rawPosts = await notion.databases.query({
+        database_id: process.env.NOTION_BLOG_DATABASE_ID as string,
+        filter: {
+            property: "status",
+            status: {
+                "equals": "Published"
+            },
+            and: [
+                {
+                    property: "slug",
+                    rich_text: {
+                        equals: slug
+                    }
+                }
+            ]
+        },
+    });
+
+    if (rawPosts.results.length < 1) return null;
+
+    let blocksRes: any = await notion.blocks.children.list({
+        block_id: rawPosts.results[0].id,
+    });
+
+    const blocks: Block[] = blocksRes.results;
+
+    return blocks;
+}
+
+export default async function PostPage({ params }: { params: { slug: string, id: string } }) {
+    const blocks = await fetchBlocks(params.slug);
+
+    if (!blocks) {
+        return <div />;
+    }
+
     return (
         <>
             <div className="flex flex-col gap-2 pt-2">
-
+                <article>
+                    <section>
+                        {blocks.map((block) => (
+                            <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+                        ))}
+                    </section>
+                </article>
             </div>
         </>
     )
+}
+
+export async function generateStaticParams() {
+    const database = await notion.databases.query({
+        database_id: process.env.NOTION_BLOG_DATABASE_ID as string,
+        filter: {
+            property: "status",
+            status: {
+                equals: "Published"
+            }
+        }
+    });
+
+    let properties: BlogPost[] = database.results.map((p: any) => p.properties);
+
+    if (properties.length < 1) return;
+
+    return properties.map((p) => ({ "slug": p.slug.rich_text[0].plain_text }));
 }
---
+++ types/block.ts
@@ -0,0 +1,95 @@
+export interface Block {
+    object: string;
+    id: string;
+    parent: Parent;
+    created_time: Date;
+    last_edited_time: Date;
+    created_by: TedBy;
+    last_edited_by: TedBy;
+    has_children: boolean;
+    children: any | undefined;
+    archived: boolean;
+    type: string;
+    // stuff 👍
+    paragraph: Paragraph;
+    heading_1: Heading;
+    heading_2: Heading;
+    heading_3: Heading;
+    bulleted_list_item: Paragraph;
+    numbered_list_item: Paragraph;
+    to_do: ToDo;
+    toggle: Toggle;
+    image: Image;
+}
+
+export interface TedBy {
+    object: string;
+    id: string;
+}
+
+export interface Paragraph {
+    rich_text: RichText[];
+    color: string;
+}
+
+export interface Heading extends Paragraph {
+    is_toggleable: boolean;
+}
+
+export interface ToDo {
+    rich_text: RichText[];
+    checked: boolean;
+    color: string;
+}
+
+export interface Toggle {
+    rich_text: RichText[];
+    color: string;
+}
+
+export interface Image {
+    caption: any[];
+    type: string;
+    file: File;
+    external: External;
+}
+
+export interface External {
+    url: string;
+}
+
+export interface File {
+    url: string;
+    expiry_time: Date;
+}
+
+export interface RichText {
+    type: string;
+    text: Text;
+    annotations: Annotations;
+    plain_text: string;
+    href: string;
+}
+
+export interface Annotations {
+    bold: boolean;
+    italic: boolean;
+    strikethrough: boolean;
+    underline: boolean;
+    code: boolean;
+    color: string;
+}
+
+export interface Text {
+    content: string;
+    link: Link;
+}
+
+export interface Link {
+    url: string;
+}
+
+export interface Parent {
+    type: string;
+    page_id: string;
+}
