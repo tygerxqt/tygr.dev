fed2347fff409ce0f44bdb0977a9a57c6176e5f4
Ty Mason <hi@tygr.dev>
2023-07-19 17:25:27 +0100

    fix: image grid spacing

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,3 +1,6 @@
+import Carousel, { CarouselItem } from "@/components/gallery/carousel";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { notion } from "@/lib/notion"
 import { ImageProps } from "@/types/image";
 import Image from "next/image";
@@ -18,19 +21,12 @@ export default async function GalleryPage() {
     return (
         <>
             <main className="mx-auto max-w-[1960px] p-4">
-                <div className="gap-4 columns-1 sm:columns-2 xl:columns-3 2xl:columns-4">
-                    {images.map((i: ImageProps, index: number) => (
-                        <Image
-                            alt={i.Name.title[0].text.content}
-                            className="mt-4 transition transform border rounded-lg border-black/10 dark:border-white/10 brightness-90 will-change-auto group-hover:brightness-110"
-                            style={{ transform: "translate3d(0, 0, 0)" }}
-                            src={i.Image.files[0].file.url}
-                            key={index}
-                            width={720}
-                            height={480}
-                            sizes="(max-width: 640px) 100vw, (max-width: 1280px) 50vw, (max-width: 1536px) 33vw, 25vw"
-                        />
-                    ))}
+                <div className="grid grid-cols-3 gap-4">
+                    {images.map((image, index) => {
+                        return (
+                            <div className="h-[400px] "><Image src={image.Image.files[0].file.url} alt={image.Name.title[0].plain_text} width={1920} height={1080} className="object-cover w-full h-full border rounded-lg border-black/10 dark:border-white/10" /></div>
+                        )
+                    })}
                 </div>
             </main>
         </>
