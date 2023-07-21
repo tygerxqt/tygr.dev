0c4b4a047fe986346413fc71b7bae096f05fe515
Ty Mason <hi@tygr.dev>
2023-07-21 14:08:27 +0100

    fix: refetch images after 1hr

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,30 +1,26 @@
-import Carousel, { CarouselItem } from "@/components/gallery/carousel";
-import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import { notion } from "@/lib/notion"
-import { ImageProps } from "@/types/image";
 import Image from "next/image";
-
-async function getImages() {
-    const raw = await notion.databases.query({
-        database_id: process.env.NOTION_GALLERY_DATABASE_ID as string,
-    });
-
-    const res: ImageProps[] = raw.results.map((p: any) => p.properties);
-
-    return res;
-}
+import { Suspense } from "react";
 
 export default async function GalleryPage() {
-    const images = await getImages();
+    const data = await fetch(`${process.env.NEXT_PUBLIC_URL}/gallery/images`, { next: { revalidate: 3600 } });
+    const images = await data.json() as {
+        src: string,
+        alt: string
+    }[];
 
     return (
         <>
             <main className="mx-auto max-w-[1960px] p-4">
-                <div className="grid grid-cols-3 gap-4">
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
                     {images.map((image, index) => {
                         return (
-                            <div className="h-[400px] "><Image src={image.Image.files[0].file.url} alt={image.Name.title[0].plain_text} width={1920} height={1080} className="object-cover w-full h-full border rounded-lg border-black/10 dark:border-white/10" /></div>
+                            <button>
+                                <div className="h-[400px]">
+                                    <Suspense fallback={<p>Loading...</p>}>
+                                        <Image src={image.src} alt={image.alt} width={1200} height={800} className="object-cover w-full h-full border rounded-lg border-black/10 dark:border-white/10" />
+                                    </Suspense>
+                                </div>
+                            </button>
                         )
                     })}
                 </div>
