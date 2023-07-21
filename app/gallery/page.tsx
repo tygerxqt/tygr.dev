24278b481901e9e75c2c4a30fddb66dd3603f355
Ty Mason <hi@tygr.dev>
2023-07-21 14:29:37 +0100

    refactor: image caching

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,12 +1,22 @@
+import { ImageProps } from "@/types/image";
 import Image from "next/image";
 import { Suspense } from "react";
 
 export default async function GalleryPage() {
-    const data = await fetch(`${process.env.NEXT_PUBLIC_URL}/gallery/images`, { next: { revalidate: 3600 } });
-    const images = await data.json() as {
-        src: string,
-        alt: string
-    }[];
+    const data = await fetch(`https://api.notion.com/v1/databases/${process.env.NOTION_GALLERY_DATABASE_ID}/query`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${process.env.NOTION_API_KEY}`,
+            accept: 'application/json',
+            'Notion-Version': '2022-06-28',
+            'content-type': 'application/json'
+        },
+        next: {
+            revalidate: 3600
+        }
+    }).then((res) => res.json()).then((json) => json.results);
+
+    const images: ImageProps[] = data.map((p: any) => p.properties);
 
     return (
         <>
@@ -17,7 +27,7 @@ export default async function GalleryPage() {
                             <button key={index}>
                                 <div className="h-[400px]">
                                     <Suspense fallback={<p>Loading...</p>}>
-                                        <Image src={image.src} alt={image.alt} width={1200} height={800} className="object-cover w-full h-full border rounded-lg border-black/10 dark:border-white/10" />
+                                        <Image src={image.Image.files[0].file.url} alt={image.Name.title[0].plain_text} width={1200} height={800} className="object-cover w-full h-full border rounded-lg border-black/10 dark:border-white/10" />
                                     </Suspense>
                                 </div>
                             </button>
