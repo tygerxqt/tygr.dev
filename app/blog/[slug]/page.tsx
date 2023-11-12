e442855b1747d309d0803e2983325506e8a9c656
Ty Mason <hi@tygr.dev>
2023-11-12 18:03:44 +0000

    refactor: use mdx-bundler and move mdx to cms (#23)

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,61 +1,123 @@
-import { allPosts } from "@/.contentlayer/generated";
 import { format, parseISO } from "date-fns";
-import { getMDXComponent } from "next-contentlayer/hooks";
 import Image from "next/image";
-import { notFound, redirect } from "next/navigation";
+import { notFound } from "next/navigation";
 import MDXComponents from "../mdx-components";
+import { Post, cms } from "@/lib/directus";
+import { readItem, readItems } from "@directus/sdk";
+import readingTime from "reading-time";
+import { getMDXComponent } from "mdx-bundler/client";
+import { bundleMDX } from "mdx-bundler";
+import remarkGfm from "remark-gfm";
+import rehypePrettyCode from "rehype-pretty-code";
+import { Metadata } from "next";
 
 export async function generateStaticParams() {
-    return allPosts.map((post) => ({
-        slug: post._raw.flattenedPath,
-    }))
+    let posts: Post[] = await cms.request(readItems("posts")).then((res) => {
+        return res.filter((post) => post.status === "published");
+    });
+
+    return posts.map((post) => {
+        return {
+            slug: post.slug
+        }
+    });
 }
 
-export const generateMetadata = ({ params }: { params: { slug: string } }) => {
-    const post = allPosts.find((post) => post._raw.flattenedPath === params.slug)
-    if (!post) throw new Error(`Post not found for slug: ${params.slug}`)
-    return {
-        title: post.title,
+export const generateMetadata = async ({ params }: { params: { slug: string } }) => {
+    const post = await cms.request(readItem("posts", params.slug)).then((res) => {
+        return res;
+    });
+
+    if (!post) return;
+
+    let metadata: Metadata = {
+        title: post.heading,
         description: post.summary,
-        image: post.image,
-        date: post.date,
-        type: 'article',
-    }
+        publisher: "tygerxqt",
+        metadataBase: new URL(process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://tygr.dev"),
+
+        openGraph: {
+            title: post.heading,
+            description: post.summary,
+            images: [
+                {
+                    url: process.env.NEXT_PUBLIC_CMS_URL + "/assets/" + post.hero,
+                    width: 800,
+                    height: 600,
+                }
+            ],
+            locale: "en_US",
+            type: "article",
+        },
+    };
+
+    return metadata;
 }
 
-export default async function PostPage({ params }: { params: { slug: string, } }) {
-    const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+export default async function PostPage({ params }: { params: { slug: string } }) {
+    const post: Post = await cms.request(readItem("posts", params.slug)).then((res) => {
+        return res;
+    });
 
-    if (!post) notFound();
+    if (!post) {
+        return notFound();
+    }
 
-    if (post.archived) redirect(`/blog/archive/${post.slug}`);
+    const formattingOptions = {
+        theme: "vitesse-light",
+        keepBackground: false,
+        onVisitLine(node: any) {
+            // Prevent lines from collapsing in `display: grid` mode, and allow empty
+            // lines to be copy/pasted
+            if (node.children.length === 0) {
+                node.children = [{ type: 'text', value: ' ' }];
+            }
+        },
+        onVisitHighlightedLine(node: any) {
+            node.properties.className.push('highlighted');
+        },
+        onVisitHighlightedWord(node: any) {
+            node.properties.className = ['word'];
+        },
+    };
 
-    const MDXContent = getMDXComponent(post.body.code);
+    const { code, frontmatter } = await bundleMDX({
+        source: post.content,
+        cwd: process.cwd(),
+        mdxOptions(options, frontmatter) {
+            // options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkGfm];
+            options.rehypePlugins = [...(options.rehypePlugins ?? []), [rehypePrettyCode, formattingOptions]]
+
+            return options;
+        },
+    });
+
+    const MDXComponent = getMDXComponent(code);
 
     return (
         <>
             <article className="p-2 py-4 mx-auto max-w-[800px]">
                 <div className="flex flex-col gap-2 pb-2">
-                    <h1 className="text-3xl font-black font-display sm:text-4xl">{post.title}</h1>
+                    <h1 className="text-3xl font-black font-display sm:text-4xl">{post.heading}</h1>
                     <div className="flex flex-row items-center justify-between gap-4">
                         <div className="flex flex-row items-center gap-2">
                             <Image src={"https://secure.gravatar.com/avatar/871c2885d0acbbc08be33547816255e3?size=512"} alt="T" aria-label="Author avatar" width={28} height={28} className="rounded-full" />
                             <p className="text-sm text-neutral-500 dark:text-neutral-400">
                                 tygerxqt • {" "}
                             </p>
-                            <time dateTime={post.date} className="items-center text-sm text-neutral-500 dark:text-neutral-400">
-                                {format(parseISO(post.date), 'LLLL d, yyyy')}
+                            <time dateTime={post.date_created} className="items-center text-sm text-neutral-500 dark:text-neutral-400">
+                                {format(parseISO(post.date_created), 'LLLL d, yyyy')}
                             </time>
                         </div>
                         <div>
                             <p className="text-sm text-neutral-500 dark:text-neutral-400">
-                                {post.readingTime}
+                                {readingTime(post.content).text}
                             </p>
                         </div>
                     </div>
-                    <Image src={post.image} alt="hero image" height={550} width={800} className="mt-4 border rounded-lg border-black/10 dark:border-white/10" />
+                    <Image src={process.env.NEXT_PUBLIC_CMS_URL + "/assets/" + post.hero} alt="hero image" height={400} width={800} className="mt-4 border rounded-lg border-black/10 dark:border-white/10" />
                 </div>
-                <MDXContent components={MDXComponents} />
+                <MDXComponent components={MDXComponents} />
             </article>
         </>
     )
--- app/blog/archive/page.tsx
+++ app/blog/archive/page.tsx
@@ -1,44 +1,48 @@
-"use client"
-
-import { allPosts } from "@/.contentlayer/generated";
 import BlogCard from "@/components/blog/card";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { compareDesc } from "date-fns";
+import { cms } from "@/lib/directus";
+import { readItems } from "@directus/sdk";
 import { ArrowLeft } from "lucide-react";
-import { Metadata } from "next";
 import Link from "next/link";
-import { useState } from "react";
+import { Suspense } from "react";
 
-export default function BlogPage() {
-    const posts = allPosts.filter(post => post.archived === true).sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
-    const [query, setQuery] = useState("");
+export default async function BlogPage() {
+    const posts = await cms.request(readItems("posts")).then((res) => {
+        return res.filter((post) => post.status === "archived");
+    });
 
     return (
         <>
             <div className="max-w-[800px] w-full flex flex-col items-start gap-2">
-                <div>
-                    <h2 className="text-2xl font-bold sm:text-3xl">
-                        ~/blog/archive
-                    </h2>
-                    <p className="text-sm">
-                        All of my archived blog posts.
-                    </p>
-                </div>
-                <div className="flex flex-row items-center justify-between w-full gap-2 pb-4">
-                    <Input className="max-w-[400px]" placeholder="Search articles..." value={query} onChange={(e) => setQuery(e.target.value.toLowerCase())} />
+                <div className="flex flex-row items-center justify-between w-full gap-2 px-2">
+                    <div>
+                        <h2 className="text-2xl font-bold sm:text-3xl">
+                            ~/blog/archive
+                        </h2>
+                        <p className="text-sm">
+                            All of my archived blog posts.
+                        </p>
+                    </div>
                     <Link href="/blog">
                         <Button size="sm" className="p-1">
                             <ArrowLeft />
                         </Button>
                     </Link>
                 </div>
-                <div className="grid grid-cols-1 gap-4 px-2 sm:px-0 sm:gap-2 sm:grid-cols-2">
-                    {posts.filter((post) => post.title.toLowerCase().includes(query) || post.summary.toLowerCase().includes(query)).map((post, idx) => (
-                        <BlogCard key={idx} post={post} archive={true} />
-                    ))}
-                </div>
-            </div>
+
+                <Suspense fallback={<p>Loading...</p>}>
+                    {posts.length === 0 && (
+                        <div className="flex flex-col items-center justify-center w-full py-8">
+                            <p className="text-neutral-500">There are no posts to show at the moment.</p>
+                        </div>
+                    )}
+                    <div className="grid grid-cols-1 gap-4 px-2 py-2 sm:px-0 sm:gap-2 sm:grid-cols-2">
+                        {posts.filter((post) => post.status === "archived").map((post, idx) => (
+                            <BlogCard key={idx} post={post} archive={true} />
+                        ))}
+                    </div>
+                </Suspense>
+            </div >
         </>
     )
 }
